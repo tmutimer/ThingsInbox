15b7f5d9cc8479047d6890d61baed82fa05b338b
Tom Mutimer <tmutimer96@gmail.com>
2020-01-17 15:48:48 +0000

    Add hint to mail-to-things EditText

--- app/src/main/java/com/android/example/thingsinbox/SettingsActivity.java
+++ app/src/main/java/com/android/example/thingsinbox/SettingsActivity.java
@@ -1,10 +1,14 @@
 package com.android.example.thingsinbox;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -26,6 +30,16 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            PreferenceManager manager = getPreferenceManager();
+            EditTextPreference emailPreference = manager.findPreference("email");
+
+            emailPreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setHint(R.string.email_hint);
+                }
+            });
         }
     }
 }
